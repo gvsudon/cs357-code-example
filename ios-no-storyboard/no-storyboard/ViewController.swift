dc87bfaffda1d2fb4a4f6b9163469df9e918380d
Hans Dulimarta <HansDulimarta@gvsu.edu>
2024-03-18 12:07:37 -0400

    Add iOS starter app which uses no Storyboard.

---
+++ ios-no-storyboard/no-storyboard/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  no-storyboard
+//
+//  Created by Hans Dulimarta on 3/18/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = .yellow
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
