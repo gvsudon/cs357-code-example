41aac82c7df12becc42cf2b3c40e68c7c334a1c7
Hans Dulimarta <HansDulimarta@gvsu.edu>
2024-03-18 12:26:53 -0400

    ios-no-storyboard: Add own viewcontroller and its .xib

---
+++ ios-no-storyboard/no-storyboard/MainVC.swift
@@ -0,0 +1,29 @@
+//
+//  MainVC.swift
+//  no-storyboard
+//
+//  Created by Hans Dulimarta on 3/18/24.
+//
+
+import UIKit
+
+class MainVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = .yellow
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
